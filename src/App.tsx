e387878876d08745c4069d96f0e262361b228d27
stefaleon <stefaleon@gmail.com>
2020-07-13 22:35:22 +0300

    add state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // Components
 import QuestionCard from './components/QuestionCard';
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
   const startTrivia = async () => {};
   const checkAnswer = async (e: React.MouseEvent<HTMLButtonElement>) => {};
   const nextQuestion = async () => {};
