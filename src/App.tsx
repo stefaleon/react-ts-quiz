74d7b93ace90f3a53e60edbc47a0bd5363b24da0
stefaleon <stefaleon@gmail.com>
2020-07-13 22:45:33 +0300

    pass the QuestionCard props

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 // Components
 import QuestionCard from './components/QuestionCard';
 
+const TOTAL_QUESTIONS = 10;
+
 const App = () => {
   const [loading, setLoading] = useState(false);
   const [questions, setQuestions] = useState([]);
@@ -23,7 +25,14 @@ const App = () => {
       </button>
       <p className='score'>Score:</p>
       <p>Loading...</p>
-      <QuestionCard />
+      <QuestionCard
+        questionNr={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      />
       <button className='next' onClick={nextQuestion}>
         Next
       </button>
