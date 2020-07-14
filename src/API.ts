cf909dc6e9b86c684c68491ceb63d77158802dd0
stefaleon <stefaleon@gmail.com>
2020-07-14 12:58:11 +0300

    export Qestion types

--- src/API.ts
+++ src/API.ts
@@ -1,3 +1,14 @@
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+};
+
+export type QuestionState = Question & { answers: string[] };
+
 export enum Difficulty {
   EASY = 'easy',
   MEDIUM = 'medium',
@@ -8,7 +19,7 @@ export const fetchQuizQuestions = async (
   amount: number,
   difficulty: Difficulty
 ) => {
-  const endpoint = `https://opentdb.com/api.php?amount=${amount}&category=22&difficulty=${difficulty}&type=multiple`;
+  const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
   const data = await (await fetch(endpoint)).json();
   console.log(data);
 };
